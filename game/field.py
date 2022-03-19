993ace13539ceb5f3b32bcae20d6b76f657bec5b
MQ <mq@4mq.xyz>
2022-03-19 21:25:12 +0100

    add field methods

--- game/field.py
+++ game/field.py
@@ -5,10 +5,30 @@ class Field():
     def __init__(self, f_id):
         self.f_id = f_id
 
+    def __str__(self):
+        return str(self.f_id)
+
+    def __repr__(self):
+        return str(self)
+
     def set_figure(self, figure, set_field=False):
+        if self.figure:
+            return False
+
         self.figure = figure
         if set_field:
             figure.set_field(self, not set_field)
 
     def remove_figure(self):
-        pass
+        figure = self.figure
+        self.figure = None
+        return figure
+
+    def get_figure(self):
+        return self.figure
+
+    def get_id(self):
+        return self.f_id
+
+    def is_empty(self):
+        return self.figure is None
