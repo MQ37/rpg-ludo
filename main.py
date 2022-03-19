789a3308e36d0b2eb51d0e389474611409f2f372
MQ <mq@4mq.xyz>
2022-03-19 21:27:00 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
 from game import Game
 
-g = Game()
-print(g.players)
+ludo = Game()
 
-f = g.fields[0]
-
-fig = g.players[0].figures[0]
-
-f.set_figure(fig, True)
-
-print(f.figure)
-print(fig)
-print(fig.field)
-print(f)
-
-fig.set_field(f, True)
-print(f.figure)
-print(fig)
-print(fig.field)
-print(f)
+ludo.main_loop()
